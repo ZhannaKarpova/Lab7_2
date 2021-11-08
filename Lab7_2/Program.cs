6a6436c2dc8ea63351fd722ed1360b89b3d2768f
ZhannaKarpova <92864174+ZhannaKarpova@users.noreply.github.com>
2021-11-08 23:27:33 +0700

    Добавьте файлы проекта.

---
+++ Lab7_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Int32.Parse(Console.ReadLine());
+            double V, S;
+            CalsСube(a, out V, out S);
+            Console.WriteLine(V);
+            Console.WriteLine(S);
+            Console.ReadKey();
+        }
+
+        static void CalsСube(int a, out double V, out double S)
+        {
+            V = Math.Pow(a, 3);
+            S = Math.Pow(a, 3)*6;
+        }
+       
+    }
+}
